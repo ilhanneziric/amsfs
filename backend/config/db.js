49387c5dccd16a503b23cb77da24408d917bc2be
ilhanneziric <ilhan.neziric@edu.fit.ba>
2022-02-27 14:48:20 +0100

    Revert "Postavke za vercel .env"
    This reverts commit de3e2fe70c37c79be77d2eb3322caec8c52036a4.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,4 @@
-// require('dotenv').config({ path: "..env" });
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
