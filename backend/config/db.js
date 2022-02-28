001f1d0d4533df5ee4744fcf8b133c821abe5923
ilhanneziric <ilhan.neziric@edu.fit.ba>
2022-02-28 21:40:51 +0100

    Promjena baze zbog napada

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ilhan:123@cluster0.13amf.mongodb.net/amsfs?retryWrites=true&w=majority", {
+        await mongoose.connect("mongodb+srv://ilhan:123@cluster0.13amf.mongodb.net/amsfs2?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         }, ()=> console.log("Connected to db!"));
